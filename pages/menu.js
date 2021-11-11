6c50d096a80fee87f8bd7673b03306f9e2e8ab07
avivashishta29 <avivashishta@gmail.com>
2021-11-11 12:43:56 +0530

    menu colors fix

--- pages/menu.js
+++ pages/menu.js
@@ -8,6 +8,17 @@ import styles from '../styles/menu.module.css'
 import Header from '../components/Header';
 import MenuComponent from '../components/MenuComponent'
 import Footer from '../components/Footer';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F2C94C",
+    },
+    secondary: {
+      main: "#fff"
+    }
+  }
+});
 
 function MenuPage() {
     const handleChange = (event, newValue) => {
@@ -24,53 +35,46 @@ function MenuPage() {
             <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"auto",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
             <br/>
             <br/>
-            <TabContext value={value}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider',width:"100%",color:"#fff" }}>
-                    <TabList variant="fullWidth" onChange={handleChange} aria-label="lab API tabs example">
-                    <Tab style={{color:"#fff"}} sx={{
-                        color: 'rgba(255, 255, 255, 0.7)',
-                        '& .Mui-selected': {
-                        color: '#fff',
-                        },
-                        '& .MuiTabs-indicator': {
-                        backgroundColor: 'orange !important',
-                        },
-                        }} label="All" value="1" />
-                    <Tab  style={{color:"#fff"}}label="Starters" value="2" />
-                    <Tab  style={{color:"#fff"}}label="Main" value="3" />
-                    <Tab  style={{color:"#fff"}}label="Drinks" value="4" />
-                    <Tab  style={{color:"#fff"}}label="Promotion" value="5" />
-                    </TabList>
-                </Box>
-                <TabPanel value="1">
-                    <h1 style={{textAlign: 'left',marginLeft:"8%"}}>All</h1>
-                    <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
-                    <MenuComponent />
-                </TabPanel>
-                <TabPanel value="2">
-                    <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Starters</h1>
-                    <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
-                    <MenuComponent />
+            <ThemeProvider theme={theme}>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider',width:"100%",color:"#fff" }}>
+                        <TabList textColor="inherit" indicatorColor="primary" variant="fullWidth" onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab   label="All" value="1" />
+                        <Tab  style={{color:"#fff"}} label="Starters" value="2" />
+                        <Tab  style={{color:"#fff"}} label="Main" value="3" />
+                        <Tab  style={{color:"#fff"}} label="Drinks" value="4" />
+                        <Tab  style={{color:"#fff"}} label="Promotion" value="5" />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1">
+                        <h1 style={{textAlign: 'left',marginLeft:"8%"}}>All</h1>
+                        <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
+                        <MenuComponent />
+                    </TabPanel>
+                    <TabPanel value="2">
+                        <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Starters</h1>
+                        <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
+                        <MenuComponent />
 
-                </TabPanel>
-                <TabPanel value="3">
-                    <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Main</h1>
-                    <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
-                    <MenuComponent />
+                    </TabPanel>
+                    <TabPanel value="3">
+                        <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Main</h1>
+                        <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
+                        <MenuComponent />
 
-                </TabPanel>
-                <TabPanel value="4">
-                    <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Drinks</h1>
-                    <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
-                    <MenuComponent />
-                </TabPanel>
-                <TabPanel value="5">
-                    <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Promotion</h1>
-                    <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
-                    <MenuComponent />   
-                </TabPanel>
-                </TabContext>
-
+                    </TabPanel>
+                    <TabPanel value="4">
+                        <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Drinks</h1>
+                        <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
+                        <MenuComponent />
+                    </TabPanel>
+                    <TabPanel value="5">
+                        <h1 style={{textAlign: 'left',marginLeft:"8%"}}>Promotion</h1>
+                        <hr style={{width: '10%',display: 'block', marginRight:"auto",marginLeft:"8%",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
+                        <MenuComponent />   
+                    </TabPanel>
+                    </TabContext>
+                </ThemeProvider>
                 <Footer/>
         </div>
     )
