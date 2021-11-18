e2a50594c351f4563fbcc8cc37ef1251ecf0ffb6
heemankv <vheemank@gmail.com>
2021-11-18 11:59:53 +0530

    carousel rework

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './styles.module.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-function Carousel() {
-    return (
-        <div className={styles.wrapper} id="home">
-            <div className={styles.leftContainer}>
-                <h1 style={{fontSize:"40px"}}>Craving for a cheesy pizza?</h1>
-                <hr style={{width: '60%',display: 'block', marginRight:"auto",marginLeft:"auto",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
-                <p style={{width:"70%"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui. Vestibulum ut felis et lorem porta congue vitae nec turpis. Aliquam quis leo at nunc tempor faucibus et ac massa.</p>
+export default class DemoCarousel extends Component {
+    render() {
+        return (
+            <div>
+                <Carousel >
+                    <div>
+                        <img src="assets/wood.png" />
+                        <p className="legend">Legend 1</p>
+                    </div>
+                    <div>
+                        <img src="assets/wood.png" />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                    <div>
+                        <img src="assets/wood.png" />
+                        <p className="legend">Legend 3</p>
+                    </div>
+                </Carousel>
             </div>
-            <div className={styles.rightContainer}>
-            </div>
-        </div>
-    )
+        );
+    }
 }
 
-export default Carousel
+// function Carousel() {
+//     return (
+//         <div className={styles.wrapper} id="home">
+//             <div className={styles.leftContainer}>
+//                 <h1 style={{fontSize:"40px"}}>Craving for a cheesy pizza?</h1>
+//                 <hr style={{width: '60%',display: 'block', marginRight:"auto",marginLeft:"auto",backgroundColor:"#F24C4C",borderColor:"#F24C4C"}}/>
+//                 <p style={{width:"70%"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui. Vestibulum ut felis et lorem porta congue vitae nec turpis. Aliquam quis leo at nunc tempor faucibus et ac massa.</p>
+//             </div>
+//             <div className={styles.rightContainer}>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Carousel
