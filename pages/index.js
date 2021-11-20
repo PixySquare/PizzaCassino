c3eabf05a9c436fadc1d9e8323bc333671f67a81
avivashishta29 <avivashishta@gmail.com>
2021-11-20 17:09:00 +0530

    background removal

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
 import Head from 'next/head'
 import React, {Component} from 'react';
-
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Carousel from '../components/Carousel'
-// import Menu from '../components/Menu'
 import AboutUs from '../components/AboutUs'
 import Footer from '../components/Footer'
 import FindUs from '../components/FindUs'
 import BlogsComponent from '../components/BlogsComponent'
 import OurTeam from '../components/OurTeam'
 import Menu from '../components/Menu'
-
 export default class Home extends Component {
 
   render(){
