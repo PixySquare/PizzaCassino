fdcad95b7b0324fb5a13cd74e5fef6871b9b17d7
heemankv <vheemank@gmail.com>
2021-11-22 22:50:32 +0530

    image border

--- common/Image/index.js
+++ common/Image/index.js
@@ -7,7 +7,9 @@ export default class index extends Component {
         return (
 
             <div onContextMenu={(e)=>{e.preventDefault()}}>
-                <Image src={this.props.src} style={this.props.style} alt={this.props.alt} width={this.props.width} height={this.props.height} className={this.props.className} />
+                <div style={{borderRadius: '25px', overflow: 'hidden'}}>
+                <Image src={this.props.src}  alt={this.props.alt} width={this.props.width} height={this.props.height} className={this.props.className} />
+                </div>
             </div>
         )
     }
