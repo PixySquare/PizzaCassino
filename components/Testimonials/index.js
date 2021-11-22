0e01f40315a6ecd0818ebc533f3fa44c223b1615
avivashishta29 <avivashishta@gmail.com>
2021-11-22 13:26:53 +0530

    data fix

--- components/Testimonials/index.js
+++ components/Testimonials/index.js
@@ -58,7 +58,7 @@ export default class index extends Component {
                               <div className={styles.testimonialWrapper}>
                                 {/* <span>{item.Rating}</span>
                                 <br /> */}
-                                <span style={{fontSize:"135%",opacity:1.0,float:"left",textAlign:"right",width:"100%"}}>{item.Comment}</span>
+                                <span style={{fontSize:"135%",opacity:1.0,textAlign:"center",width:"100%"}}>{item.Comment}</span>
                                 <br />
                                 <br/>
                                 <span style={{fontSize:"100%",opacity:0.8,float:"left",textAlign:"right",width:"90%"}}> ~ {item.Writer}</span>
