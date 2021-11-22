25190a74f9e329d273ec543f8724ca3cbef29c05
heemankv <vheemank@gmail.com>
2021-11-22 22:19:57 +0530

    check

--- components/TeamMembers/index.js
+++ components/TeamMembers/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import styles from './styles.module.css'
 import Image from 'next/image'
 import src from '../../public/assets/teampic.png'
-function TeamMembers() {
+function TeamMembers(props) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.image}>
-                <Image src={src}/>
+                <Image src={props.data.image} alt={"team"}/>
             </div>
             <div className={styles.text}>
-                <p style={{color:"#F2C94C",fontSize:"150%"}}>Loreum Ipsum</p>
-                <p>More Loreum Ipsum</p>
+                <p style={{color:"#F2C94C",fontSize:"150%"}}>{props.data.name}</p>
             </div>
         </div>
     )
