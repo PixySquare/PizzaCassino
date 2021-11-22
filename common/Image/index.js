ad9ce5760d6b427e0fe67d04cdb29ec6918ae3c0
heemankv <vheemank@gmail.com>
2021-11-22 23:58:37 +0530

    minor

--- common/Image/index.js
+++ common/Image/index.js
@@ -8,7 +8,7 @@ export default class index extends Component {
 
             <div onContextMenu={(e)=>{e.preventDefault()}}>
                 <div style={{borderRadius: '25px', overflow: 'hidden'}}>
-                <Image src={this.props.src}  alt={this.props.alt} width={this.props.width} height={this.props.height} className={this.props.className} />
+                <Image src={this.props.src} style={this.props.style} alt={this.props.alt} width={this.props.width} height={this.props.height} className={this.props.className} />
                 </div>
             </div>
         )
