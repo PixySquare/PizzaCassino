7326ab06a8de27aa6695cdb5baef609ff81f7c4f
avivashishta29 <avivashishta@gmail.com>
2021-11-23 15:14:09 +0530

    mobile ui fix

--- common/Image/index.js
+++ common/Image/index.js
@@ -7,7 +7,7 @@ export default class index extends Component {
         return (
 
             <div onContextMenu={(e)=>{e.preventDefault()}}>
-                <div style={{borderRadius: '25px', overflow: 'hidden'}}>
+                <div style={{borderRadius: '5px', overflow: 'hidden'}}>
                 <Image src={this.props.src} style={this.props.style} alt={this.props.alt} width={this.props.width} height={this.props.height} className={this.props.className} />
                 </div>
             </div>
