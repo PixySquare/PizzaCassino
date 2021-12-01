9f01c11f62bc7457ffed3b927eea15dd9672c306
heemankv <vheemank@gmail.com>
2021-12-01 16:44:45 +0530

    Find us done

--- components/FindUs/index.js
+++ components/FindUs/index.js
@@ -8,13 +8,14 @@ function FindUs() {
   return (
     <div className={styles.mainwrapper} style={{scrollMarginTop: 80}}
       id="findus">
-      <h1 style={{textAlign: 'center'}}>{FindUsContent.heading}</h1>
-      <hr style={{width: '35%', display: 'block', marginRight: 'auto',
-        marginLeft: 'auto', backgroundColor: '#F24C4C',
-        borderColor: '#F24C4C'}} />
+      <h1 className={styles.h1} style={{textAlign: 'center'}}>
+        {FindUsContent.heading}</h1>
+      <hr className={styles.hr}
+        style={{width: '45%', display: 'block', marginRight: 'auto',
+          marginLeft: 'auto', backgroundColor: '#F24C4C',
+          borderColor: '#F24C4C'}} />
       <div className={styles.wrapper}>
         <div className={styles.rightContainer}>
-          <p>{FindUsContent.location1.description}</p>
           <span style={{display: 'flex', alignItems: 'center',
             justifyContent: 'center'}}>
             <RoomIcon fontSize="large" style={{marginRight: 10}}/>
@@ -41,9 +42,9 @@ function FindUs() {
         </div>
 
         <div className={styles.rightContainer}>
-          <p>{FindUsContent.location2.description}</p>
           <span style={{display: 'flex', alignItems: 'center',
-            justifyContent: 'center'}}><RoomIcon fontSize="large"
+            justifyContent: 'center', width: '100%'}}>
+            <RoomIcon fontSize="large"
               style={{marginRight: 10}}/>
             <h4>{FindUsContent.location2.locationtext}</h4></span>
           <span style={{display: 'flex', alignItems: 'center',
