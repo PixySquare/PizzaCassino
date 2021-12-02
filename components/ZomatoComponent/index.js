156ffc0dfaac9b9945419e56ce607406d12ab0db
avivashishta29 <avivashishta@gmail.com>
2021-12-03 02:49:56 +0530

    zomato mobile fix

--- components/ZomatoComponent/index.js
+++ components/ZomatoComponent/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
 function ZomatoComponent() {
+  const [text, setText] =
+  useState('We Are Available At');
+  useEffect(()=> {
+    if (process.browser) {
+      if (window.innerWidth < 800) {
+        setText('Order Online At');
+      }
+    }
+  }, [text]);
   return (
     <div className={styles.wrapper}>
-      <p style={{textAlign: 'left', width: '70%', marginRight: 20}}>
-          We Are Available At</p>
+      <p style={{whiteSpace:"nowrap",textAlign: 'left', width: '100%', marginRight: 20}}>
+          {text}</p>
       <div style={{display: 'flex', flexDirection: 'row'}}>
         <div className={styles.zomatoButton}>
           <a href={'http://zomato.com'}>ZOMATO</a>
